b4f98f6e176e7c3b0dcc6b368c2a0b9eb67b9bc7
ben <benjaminalexanderfox11@gmail.com>
2022-07-19 14:45:55 -0400

    react branch

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,38 @@
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import { useEffect, useState } from 'react'
 import Header from './Components/Header'
 import Footer from './Components/Footer'
+import Post from './Components/Post'
 function App() {
+  const [posts, setPosts] = useState([])
+  useEffect(() => {
+    let headersList = {
+      Accept: '*/*',
+      'User-Agent': 'Thunder Client (https://www.thunderclient.com)',
+    }
+
+    fetch('/posts', {
+      method: 'GET',
+      headers: headersList,
+    }).then((res) => {
+      console.log(res)
+      setPosts(res.data)
+    })
+  }, [])
+
   return (
     <div className="App">
       <Header />
-      hello
+      <Routes>
+        {posts.map((post) => (
+          <Route
+            key={post._id}
+            path={`/posts/${post._id}`}
+            render={() => <Post />}
+          ></Route>
+        ))}
+      </Routes>
       <Footer />
     </div>
   )
---
+++ client/src/Components/Post.js
@@ -0,0 +1,13 @@
+//make a quick post component
+// Language: javascript
+// Path: client/src/Components/Post.js
+
+const Post = (props) => {
+  return (
+    <div>
+      <h2>{props}</h2>
+    </div>
+  )
+}
+
+export default Post
